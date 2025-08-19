c3816d4b3948ba5e63ee60be34dcfcc2fb26d549
santosflavim <145395434+santosflavim@users.noreply.github.com>
2025-08-19 14:53:07 -0300

    19/08

--- main.js
+++ main.js
@@ -31,12 +31,12 @@ const numeros = '0123456789';
 const simbolos = '!@%?*#+/';
 gerasenha();
 
-function gerasenha(){
-     let senha = '';
-     for(let i=0; i<tamanhoSenha; i++){
-         let numeroAleatorio = Math.random()*letrasMaiusculas.length;
-         numeroAleatorio = Math.floor(numeroAleatorio);
-         senha = senha + letrasMaiusculas[numeroAleatorio];
-     }
-     campoSenha.value = senha;
+function gerasenha() {
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha; i++) {
+        let numeroAleatorio = Math.random() * letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + letrasMaiusculas[numeroAleatorio];
+    }
+    campoSenha.value = senha;
 }
