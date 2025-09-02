06a0e44b318f4fa8b729961e2d258492d66261dd
santosflavim <145395434+santosflavim@users.noreply.github.com>
2025-09-02 14:31:19 -0300

    02/09

--- main.js
+++ main.js
@@ -65,7 +65,7 @@ function gerasenha() {
 }
 
 function classificaSenha(tamanhoAlfabeto) {
-  let entropia = tamanhoSenha*Math.log2(tamanhoSenha);
+    let entropia = tamanhoSenha * Math.log2(tamanhoSenha);
 
 
     forcaSenha.classList.remove('fraca', 'media', 'forte');
@@ -73,12 +73,12 @@ function classificaSenha(tamanhoAlfabeto) {
         forcaSenha.classList.add('forte');
     } else if (entropia > 35 && entropia < 57) {
         forcaSenha.classList.add('media');
-    } else if (entropia<= 35) {
+    } else if (entropia <= 35) {
         forcaSenha.classList.add('fraca');
     }
-const valorEntropia = document.querySelector('.entropia');
-valorEntropia.textContent = "Um computador pode levar até"
-Math.floor(2**entropia/(100e6*60*60*24)) + "dias para descobrir essa senha"
+    const valorEntropia = document.querySelector('.entropia');
+    valorEntropia.textContent = "Um computador pode levar até"
+    Math.floor(2 ** entropia / (100e6 * 60 * 60 * 24)) + "dias para descobrir essa senha"
 }
 
 
